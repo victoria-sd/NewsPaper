93efa708a2facfdaca810751f7ef5f08eee7cb21
Victoria <email@example.ru>
2024-09-28 08:06:51 +0300

    missing files

--- NewsPapers/urls.py
+++ NewsPapers/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('news/', include('news.urls')),
+    path('posts/', include('news.urls')),
 ]
