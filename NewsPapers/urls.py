c1fac47ecb2564a88328983b375bca2e5ba8331f
Victoria <email@example.ru>
2024-09-28 07:53:25 +0300

    Init Skillfactory

--- NewsPapers/urls.py
+++ NewsPapers/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('news/', include('news.urls')),
 ]
