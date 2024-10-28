b8bf5422c696f0da603b38d9c5abf7851a647c80
Victoria <email@example.ru>
2024-10-28 20:45:35 +0300

    added signals

---
+++ news/signals.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Post
+
+
+@receiver(post_save, sender=Post)
+def post_created(instance, created, **kwargs):
+    if not created:
+        return
+
+    emails = User.objects.filter(
+        subscriptions__category=instance.category
+    ).values_list('email', flat=True)
+
+    subject = f'Новая статья в категории {instance.category}'
+
+    text_content = (
+        f'Статья: {instance.heading}\n'
+        f'Ссылка на товар: http://127.0.0.1:8000{instance.get_absolute_url()}'
+    )
+    html_content = (
+        f'Товар: {instance.heading}<br>'
+        f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
+        f'Ссылка на статью</a>'
+    )
+    for email in emails:
+        msg = EmailMultiAlternatives(subject, text_content, None, [email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
